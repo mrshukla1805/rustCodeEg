610bb00c1eab2b0fdfa82e5733b255ae22c523e8
mrshukla1805 <nageshshukla1805@gmail.com>
2024-08-19 16:43:12 +0530

    removed files

--- rust/lang/security/injection/mysql-sqli.rs
+++
@@ -1,9 +0,0 @@
-fn mysql_query_first() {
-    //ruleid: mysql-sqli
-    conn.query_first(format!("SELECT {} from tmp", "x"));
-}
-
-fn mysql_query_first1() {
-    //ruleid: mysql-sqli
-    conn.query_first(x + y);
-}
--- rust/lang/security/injection/sqlx-sqli.rs
+++
@@ -1,9 +0,0 @@
-fn sqlx_sqli() {
-    //ruleid: sqlx-sqli
-    sqlx::query!(format!("SELECT {} from tmp", "x"));
-}
-
-fn sqlx_sqli1() {
-    //ruleid: sqlx-sqli
-    sqlx::query!(x + y);
-}
